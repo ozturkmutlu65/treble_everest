3b732bcbaef174654f2aace0edf9f6ef8e1f14e5
Christopher A. Williamson <home@chrisaw.com>
2023-12-25 18:54:11 +0000

    Add quick script to automate gapps-free build

---
+++ build-rom.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Quick 'N dirty script to build gapps-free VoltageOS for arm64
+
+repo init -u https://github.com/VoltageOS/manifest.git -b 14 --depth=1
+
+git lfs install
+git clone https://github.com/ahnet-69/treble_manifest.git -b voltage-14 .repo/local_manifests
+
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo forall -c git lfs pull
+
+./apply-patches.sh . trebledroid
+./apply-patches.sh . ponces
+./apply-patches.sh . personal
+
+pushd device/phh/treble
+bash generate.sh Voltage
+popd
+
+CCACHE_COMPRESS=1
+CCACHE_MAXSIZE=50G
+USE_CCACHE=1
+export CCACHE_COMPRESS CCACHE_MAXSIZE USE_CCACHE
+
+. build/envsetup.sh
+ccache -M 50G -F 0
+lunch treble_arm64_bvN-userdebug
+make systemimage -j$(nproc --all)
