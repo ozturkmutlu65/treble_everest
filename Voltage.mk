66294352f162e24b9238c94adf5b89f6fef6f681
Christopher A. Williamson <home@chrisaw.com>
2024-01-21 10:55:22 +0000

    Fix 720p bootanimation

--- Voltage.mk
+++ Voltage.mk
@@ -6,7 +6,7 @@ $(call inherit-product, device/voltage/sepolicy/common/sepolicy.mk)
 -include vendor/voltage/build/core/config.mk
 
 # Bootanimation
-TARGET_BOOT_ANIMATION_RES := 1280 # 720x1280 - 720p
+TARGET_BOOT_ANIMATION_RES := 720
 
 # Kernel
 TARGET_NO_KERNEL_IMAGE := true
