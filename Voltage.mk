1b84f3189d73b553e88a88ed9831999915c5b85c
Christopher A. Williamson <home@chrisaw.com>
2024-01-21 10:25:51 +0000

    Fix mtk bootloop, read-only patch, texfat bug

--- Voltage.mk
+++ Voltage.mk
@@ -1,19 +1,20 @@
 $(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+$(call inherit-product, vendor/voltage/config/common.mk)
 $(call inherit-product, vendor/voltage/config/BoardConfigSoong.mk)
 $(call inherit-product, vendor/voltage/config/BoardConfigVoltage.mk)
 $(call inherit-product, device/voltage/sepolicy/common/sepolicy.mk)
 -include vendor/voltage/build/core/config.mk
 
-TARGET_NO_KERNEL_OVERRIDE := true
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1280 # 720x1280 - 720p
+
+# Kernel
 TARGET_NO_KERNEL_IMAGE := true
-SELINUX_IGNORE_NEVERALLOWS := true
-TARGET_BOOT_ANIMATION_RES := 1280
+TARGET_NO_KERNEL_OVERRIDE := true
 
-TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
-BOARD_EXT4_SHARE_DUP_BLOCKS := true
-
+# OTA
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.system.ota.json_url=https://raw.githubusercontent.com/cawilliamson/treble_voltage/14.0/ota.json
 
-PRODUCT_PACKAGE_OVERLAYS += \
-   $(LOCAL_PATH)/overlay-voltage
+# SELinux
+SELINUX_IGNORE_NEVERALLOWS := true
