1ff648875d7704e339cad7dafbcce399535f1e87
ozturkmutlu65 <155987147+ozturkmutlu65@users.noreply.github.com>
2024-07-28 18:44:38 +0300

    Everest vanilla make

--- configs/everest-vanilla.mk
+++ configs/everest-vanilla.mk
@@ -1,10 +1,13 @@
-$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
-$(call inherit-product, vendor/voltage/config/BoardConfigVoltage.mk)
-$(call inherit-product, device/voltage/sepolicy/common/sepolicy.mk)
--include vendor/voltage/build/core/config.mk
+$(call inherit-product, vendor/everest/config/common_full_phone.mk)
+$(call inherit-product, vendor/everest/config/BoardConfigEverest.mk)
+$(call inherit-product, device/everest/sepolicy/common/sepolicy.mk)
+-include vendor/everest/build/core/config.mk
+
+EVEREST_MAINTAINER := mrgebesturtle
+WITH_GAPPS := false
 
 # Bootanimation (force 720p - 720x1280)
-TARGET_BOOT_ANIMATION_RES := 1280
+TARGET_BOOT_ANIMATION_RES := 720
 
 # Kernel
 TARGET_NO_KERNEL_IMAGE := true
